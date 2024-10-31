e1f12f66106516198f44f2c5fe1ffc33fcbf8706
CorneliusKibetDC <kapslogo18@gmail.com>
2024-10-31 12:14:51 +0300

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import About from "./About.js";
+import Home from "./Home.js";
+import NavBar from "./NavBar.js";
+
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar/>
+      <Home/>
+      <About/>
     </div>
   );
 }
